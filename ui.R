d0eebf2ec8fb2923b90e5d78061e4469bb98a0cd
philferriere <pferriere@hotmail.com>
2016-05-18 13:36:01 -0700

    Use different package loading code based on hosting platform

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@
 ## Client-side UI code
 ## by Phil Ferriere
 
+# Use this if you're running this app locally or on your own Shiny server
 deps = c("shiny", "shinydashboard", "markdown", "formattable", "shinyBS", "mscsweblm4r")
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
@@ -11,6 +12,14 @@ for (dep in deps){
 for (dep in deps){
   library(dep, character.only = TRUE)
 }
+
+# Use this is you're hosting this app on shinyapps.io: they don't allow calls to install.packages()
+# library(shiny)
+# library(shinydashboard)
+# library(markdown)
+# library(formattable)
+# library(shinyBS)
+# library(mscsweblm4r)
 
 # Configure mscsweblm4r
 Sys.setenv(MSCS_WEBLANGUAGEMODEL_CONFIG_FILE = "./data/.mscskeys.json")
